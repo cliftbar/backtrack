a329417262c1185c6a7f409417fea678d0931a4e
cliftbar <cwbarclift@gmail.com>
2024-11-02 13:06:58 -0700

    sqid endpoint

--- src/backtrack/routes/keys.py
+++ src/backtrack/routes/keys.py
@@ -7,5 +7,10 @@ keys_router: APIRouter = APIRouter()
 
 
 @keys_router.get("/profile/{key}", response_class=PlainTextResponse)
-def profile(key: str, track_id: str):
+async def profile(key: str, track_id: str):
     return controller.make_profile(key, track_id)
+
+@keys_router.get("/profile/sqid")
+async def get_sqid():
+    new_id: str = await controller.get_next_squid()
+    return {"key": new_id}
